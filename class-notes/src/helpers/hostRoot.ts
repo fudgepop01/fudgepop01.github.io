8739a69f9ec7ae79032a794f7602672d25729225
fudgepop01 <fudgepop01@gmail.com>
2021-01-28 17:35:04 -0500

    updated path

--- class-notes/src/helpers/hostRoot.ts
+++ class-notes/src/helpers/hostRoot.ts
@@ -1,4 +1,4 @@
 export const hostRootAddr =
   (window.location.hostname === "localhost")
   ? "./pages/"
-  : "https://raw.githubusercontent.com/BrawlRE/BrawlRE.github.io/main/public/pages/"
+  : "https://raw.githubusercontent.com/fudgepop01/fudgepop01.github.io/class-notes/public/pages/"
